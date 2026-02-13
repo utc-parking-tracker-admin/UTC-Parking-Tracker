a4a8a53ae18a28662a41ee88edd1c15be935986b
sophia71205 <redmax35@icloud.com>
2026-02-13 22:10:15 +0000

    Changes to streamlit_app.py: Added link to map popup
    Additional changes: added support for database connection in dev environment (Codespaces)

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,10 @@ def main():
     # Display a grid of spaces (?)
 
     # location of lot
+    address_link = "<a href='https://maps.app.goo.gl/EgeZWvWBKXv84muh6' target='blank'>Get Directions</a>"
     m = folium.Map(location=[35.046235, -85.2967971], zoom_start=18)
     folium.Marker(
-        [35.046235, -85.2967971], popup="Lot 12", tooltip="Lot 12"
+        [35.046235, -85.2967971], popup=address_link, tooltip="Lot 12"
     ).add_to(m)
 
     # render map
@@ -30,6 +31,7 @@ def main():
 
     # return time stamp
     time = db_query(db, "Lot 12", "Totals")["time"]
+    time = time.strftime("%Y-%m-%d %H:%M:%S")
     st.write("Data last updated: " + time)
 
     st.write("Created by Ashley Carrera, Sophia Duke, Samuel Hunt, and Nathan Parnaby")
@@ -48,9 +50,9 @@ def db_connection():
 # added cache tag to keep from always reading during dev
 # will eventually change to read every x seconds
 @st.cache_data
-def db_query(db:firestore, collection:str, document:str):
+def db_query(_db:firestore, collection:str, document:str):
     # reference to collection and document
-    c = db.collection(collection)
+    c = _db.collection(collection)
     doc = c.document(document)
 
     # return the data as a dict
