3169ee9fa24cf3b2897d26001caa3a7d0ca0f8ff
samuelhunt12 <rrr536@mocs.utc.edu>
2026-04-07 10:55:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ def db_connection():
 
 # added cache tag to keep from always reading during dev
 # will eventually change to read every x seconds
-@st.cache_data
+
 def db_query(_db:firestore, collection:str, document:str):
     # reference to collection and document
     c = _db.collection(collection)
